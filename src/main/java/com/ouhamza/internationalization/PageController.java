0746881a6da7ec19cb3409374ac5bbc3ff956f72
ouhamzalhss <ouhamza.web.pro@gmail.com>
2020-06-24 14:10:34 +0100

    update controller

--- src/main/java/com/ouhamza/internationalization/PageController.java
+++ src/main/java/com/ouhamza/internationalization/PageController.java
@@ -1,5 +1,9 @@
 package com.ouhamza.internationalization;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,10 +11,16 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @author Lhouceine OUHAMZA
  */
 @Controller
+
 public class PageController {
+
+
+    @Autowired
+    MessageSource messageSource;
 
 @GetMapping("/international")
 public String getInternationalPage() {
+    System.out.println(messageSource.getMessage("greeting", null, LocaleContextHolder.getLocale()));
     return "international";
 }
 
